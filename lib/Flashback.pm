aed75509f4b4f182348a125fe99d1135723a080b
Jonathan Hall <jhall@efolder.net>
2015-05-28 12:23:54 -0500

    Do not require auth for js, css, and images.

--- lib/Flashback.pm
+++ lib/Flashback.pm
@@ -5,7 +5,7 @@ our $VERSION = '0.1';
 
 hook 'before' => sub {
     # If user is not logged in, redirect to the login page
-    if (!session('user') && request->path_info !~ m{^/auth}) {
+    if (!session('user') && request->path_info !~ m{^/(auth|javascripts|css|images)}) {
         status 401; # Not authorized
         forward '/auth', { continue => request->path_info };
     }
