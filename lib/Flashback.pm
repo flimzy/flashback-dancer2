eab3b9fc4e6465543d3ea0468738b357ae94613f
Jonathan Hall <jhall@efolder.net>
2015-05-28 11:30:06 -0500

    Fixes #1 - Add basic redirection for unauthorized users.

--- lib/Flashback.pm
+++ lib/Flashback.pm
@@ -3,8 +3,20 @@ use Dancer2;
 
 our $VERSION = '0.1';
 
+hook 'before' => sub {
+    # If user is not logged in, redirect to the login page
+    if (!session('user') && request->path_info !~ m{^/auth}) {
+        status 401; # Not authorized
+        forward '/auth', { continue => request->path_info };
+    }
+};
+
 get '/' => sub {
     template 'index';
+};
+
+get '/auth' => sub {
+    template 'auth';
 };
 
 true;
